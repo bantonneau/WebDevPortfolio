d68180431fd1589a8c7e0bddc0fefe6083777c43
benos <benantonn@gmail.com>
2023-08-09 11:21:40 -0500

    update nav styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
-import { LinkedIn, GitHub, Email, Assignment } from '@mui/icons-material'
+import { LinkedIn, GitHub} from '@mui/icons-material'
 
 function Navbar() {
     return (
         <nav className='navbar'>
             <div className='navbar-logo'>
-                <a href='/'>antonneau web dev</a>
+                <a href='/'>Bennett Antonneau</a>
             </div>
             <div className='linkIcons'>
                 <Link to="https://www.linkedin.com/in/benantonn/" target='_blank'>
                     <LinkedIn />
                 </Link>
-                <Link to='mailto:benantonn@gmail.com' target='_blank'>
-                    <Email />
-                </Link>
                 <Link to='https://github.com/bantonneau' target='_blank'>
                     <GitHub className='githubIcon' />
                 </Link>
-                <Link to="/resume">
-                    <Assignment />
-                </Link>
+                <a>Resume</a>
+                <a>Contact</a>
             </div>
         </nav>
     )
