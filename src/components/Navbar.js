3468e2aacf6b4a1a9548c6b9420be2196590efe1
benos <benantonn@gmail.com>
2023-08-11 23:37:28 -0500

    fix weird safari display bug on icon borders

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,45 +3,46 @@ import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
 import { LinkedIn, GitHub, Email, Article } from '@mui/icons-material'
 import Logo from '../assets/logo.png'
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
 function Circle() {
     return (
-        <svg width="40" height="40" className='iconBorder'>
-            <circle cx="20" cy="20.3" r="16" fill="none" stroke="#00D7D7" strokeWidth="2" />
+        <svg width="40" height="40" className={isSafari ? 'iconBorder safari' : 'iconBorder'}>
+            <circle cx="20" cy="20.3" r="16" fill="none" stroke="#00D7D7" />
         </svg>
     );
 }
 
 function RoundedSquare() {
     return (
-        <svg width="30" height="30" className='iconBorder'>
-            <rect x="1" y="1" width="28" height="28" rx="4" ry="4" fill="none" stroke="#00D7D7" strokeWidth="2" />
+        <svg width="30" height="30" className={isSafari ? 'iconBorder safari' : 'iconBorder'}>
+            <rect x="1" y="1" width="28" height="28" rx="4" ry="4" fill="none" stroke="#00D7D7" />
         </svg>
     );
 }
 
 function EmailOutline() {
     return (
-        <svg width="35" height="27" className='iconBorder'>
-            <rect x="1" y="1" width="31" height="25" rx="4" ry="4" fill="none" stroke="#00D7D7" strokeWidth="2" />
+        <svg width="35" height="27" className={isSafari ? 'iconBorder safari' : 'iconBorder'}>
+            <rect x="1" y="1" width="31" height="25" rx="4" ry="4" fill="none" stroke="#00D7D7" />
         </svg>
     );
 }
 
 function ResumeOutline() {
     return (
-        <svg width="30" height="33" className='iconBorder'>
-            <rect x="1" y="1" width="28" height="28" rx="4" ry="4" fill="none" stroke="#00D7D7" strokeWidth="2" />
+        <svg width="30" height="33" className={isSafari ? 'iconBorder safari' : 'iconBorder'}>
+            <rect x="1" y="1" width="28" height="28" rx="4" ry="4" fill="none" stroke="#00D7D7" />
         </svg>
     );
 }
 function Navbar() {
     return (
         <nav className='navbar'>
-            <div className='navbarLogo'>
+            <section className='navbarLogo'>
                 <img src={Logo} alt="Company Logo" className='navbarImage' />
                 <h1 href='/' className=''>Bennett Antonneau</h1>
-            </div>
+            </section>
             <ul className='navLinks'>
                 <li>
                     <Link to="https://www.linkedin.com/in/benantonn/" target='_blank' className='LInavIcon'>
@@ -52,7 +53,7 @@ function Navbar() {
                 <li>
                     <Link to='https://github.com/bantonneau' target='_blank' className='GHnavIcon'>
                         <GitHub className='githubIcon' />
-                        <Circle />
+                        <Circle  />
                     </Link>
                 </li>
                 <section className='navButtons'>
