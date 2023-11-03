92eeb491745f1c3dbc7ee2361ca84de295bd081f
benos <benantonn@gmail.com>
2023-11-03 10:49:09 -0500

    update skill list format

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,72 +1,26 @@
 import React from 'react'
 import '../styles/Skills.css'
 
-export const technicalSkills = [
-    "HTML/CSS",
-    "JavaScript/JQuery",
-    "Responsive Web Design",
-    "React",
-    "Node.js",
-    "Express.js",
-    "MySQL",
-    "Sequelize",
-    "NoSQL",
-    "MongoDB",
-    "User Authentication",
-    "Session Storage",
-    "OAuth",
-    "Bcrypt",
-    "Restful APIs",
-    "JSON",
-    "AJAX",
-    "GraphQL",
-    "Handlebars",
-    "Bootstrap",
-    "SASS",
-    "MVC Framework",
-    "Agile Development",
-    "Git",
-    "Heroku",
-    "Shell Scripting",
-    "Unit Testing",
-    "Linting",
-    "NPM",
-    "Jest",
-  ];
-  
-  const professionalSkills = [
-    "Attention to Detail",
-    "Creativity",
-    "Problem Solving",
-    "Communication",
-    "Teamwork",
-    "Leadership",
-    "Relationship Building",
-    "Conflict Resolution",
-    "Adaptability",
-    "Integrity",
-    "Resilience"
-];
 
 function Skills() {
     return (
         <section className='skills row'>
-            <h1>Technical Skills</h1>
-            <ul className='skillsContainer'>
-                {technicalSkills.map((skill, index) => (
-                    <li className="skillTile" key={index}>
-                        {skill}
-                    </li>
-                ))}
-            </ul>
-            <h1>Professional Skills</h1>
-            <ul className='skillsContainer'>
-                {professionalSkills.map((skill, index) => (
-                    <li className="skillTile" key={index}>
-                        {skill}
-                    </li>
-                ))}
-            </ul>
+            <div className='skillsLanguages skillsTile'>
+                <h2>Languages</h2>
+                <p>Java, HTML/CSS, Javascript, Typescript, SQL</p>
+            </div>
+            <div className='skillsFrameworks skillsTile'>
+                <h2>Frameworks and Libraries</h2>
+                <span className='skillListItem'>Spring</span> 
+                <span className='skillListItem'>React</span>  
+                <span className='skillListItem'>Node.js</span> 
+                <span className='skillListItem'>Express.js</span>  
+                    {/* React, Node.js, Express.js, Mongoose, Next.js, Sequelize, Tailwind</p> */}
+            </div>
+            <div className='skillsTools skillsTile'>
+                <h2>Tools, Platforms, and Concepts</h2>
+                <p>Git, Docker, Kubernetes, RESTful APIs, Web Accessibility Standards, Azure, Agile Development, Yarn, Npm, Maven, Web Responsive Design, Vitest, Jira</p>
+            </div>
         </section>
     )
 }
