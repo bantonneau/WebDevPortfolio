db6aed92a2d60ecd0315533a8a810e6f7017f09a
benos <benantonn@gmail.com>
2023-11-03 11:18:58 -0500

    image overlay + hover experiment

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,12 +3,13 @@ import '../styles/Projects.css'
 import { projectList } from "../helpers/ProjectList";
 import { Link } from 'react-router-dom';
 import { GitHub } from '@mui/icons-material'
+import commonCents from '../assets/projectPics/commonCents.png'
 
 function Circle() {
   return (
-      <svg width="40" height="40">
-          <circle className='GHprojBorder' cx="20" cy="20.3" r="16" strokeWidth='3px'/>
-      </svg>
+    <svg width="40" height="40">
+      <circle className='GHprojBorder' cx="20" cy="20.3" r="16" strokeWidth='3px' />
+    </svg>
   );
 }
 
@@ -18,16 +19,20 @@ function Projects({ projectRefs }) {
       <section>
         <h1>Development Experience</h1>
       </section>
+      <div className='projectPicContainer'>
+        <img src={commonCents} alt="Featured image of the 'Common Cents' project" className='projectPic' />
+      </div>
+
       <ul>
         {projectList.map((project, index) => (
           <li key={index} ref={projectRefs[index]}>
             <section className='projectTitle'>
               <p className='strongBlue' style={{ textDecoration: 'none' }}><strong>{project.name}</strong></p>
               <div className='projectButtons'>
-                  <Link to={project.github} target='_blank' className='GHprojIcon'>
-                    <GitHub className='githubIcon' />
-                    <Circle />
-                  </Link>
+                <Link to={project.github} target='_blank' className='GHprojIcon'>
+                  <GitHub className='githubIcon' />
+                  <Circle />
+                </Link>
                 {project.demo ? <button className='projButton' onClick={() => window.open(project.demo, '_blank')}>DEMO</button> : ""}
                 {project.live ? <button className='projButton' onClick={() => window.open(project.live, '_blank')}>LIVE</button> : ""}
               </div>
